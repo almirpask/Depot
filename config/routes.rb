0d8b4a6ef0254d2f1a0c04ab2d80e5d1f02a53bc
almirpask <almirpask@hotmail.com>
2018-03-25 12:44:51 -0300

    Add I18n to the project

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,17 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store_index'
 
   # resources :products
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do 
+    resources :orders 
+    resources :line_items 
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
